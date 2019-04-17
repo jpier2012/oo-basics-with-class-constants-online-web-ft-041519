53292cdaf3c202ff0ef9a8b3c8f807b0daa1a25f
James Pier <jpier2012@gmail.com>
2019-04-17 11:57:37 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,15 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
   end
 
   def cobble
@@ -11,4 +17,21 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
+  end
+
 end
+
+nike = Shoe.new("nike")
+
+nike.brand = "Adidas"
+nike.brand = "Asics"
+nike.brand = "New Balance"
+nike.brand = "Asics"
+nike.brand = "New Balance"
+
+
+puts Shoe::BRANDS
